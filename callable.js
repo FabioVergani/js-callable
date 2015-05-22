ee96e48e10ef51cbbf2a2a81aa2ef81d944e606b
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-22 22:15:07 +0200

    Update callable.js

--- callable.js
+++ callable.js
@@ -1,3 +1,16 @@
+var shorten=(function(e){
+	var o=e.prototype,b=o.bind,c=(b=b.call(b,b))(o.call);
+	o=console;
+	return {bind:b,call:c,log:c(o.log,o)};
+})(Function);
+
+var log = shorten.log;
+log('hello world'); // --> "hello world"
+
+http://calje.eu/index.php/function-bind-bindfunction-bindfunction-call/
+
+
+
 var callable=(function(o){var b=o.bind;return b.call(b,o.call);})(Function.prototype);
 var hasOwn= callable(Object.prototype.hasOwnProperty);
 console.log(hasOwn({xx:123},'xx'));
