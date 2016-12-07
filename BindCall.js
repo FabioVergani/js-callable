f9d1333803471796680e2c9fa76d1ab07fe5ff17
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-12-07 18:11:42 +0100

    Update BindCall.js

--- BindCall.js
+++ BindCall.js
@@ -2,3 +2,12 @@
 e=Function.prototype;
 BindCall=e.bind.bind(e.call);
 console.dir(BindCall);
+
+es.:
+
+
+var e=Function.prototype;
+BindCall=e.bind.bind(e.call);
+//console.dir(BindCall);
+var f=BindCall(document.createElement,document,'img')
+console.dir(f());
